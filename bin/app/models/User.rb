6c0b0e8ef27f240f73986b504be292fcca67d31b
PollyCR <polly.c.reynolds@gmail.com>
2019-08-19 11:00:53 +0100

    database amends

--- bin/app/models/User.rb
+++ bin/app/models/User.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+has_many: favourite_sources 
+has_many: favourite_articles 
 end
--- db/migrate/001_create_user.rb
+++ db/migrate/001_create_user.rb
@@ -1,8 +1,8 @@
 class CreateUser < ActiveRecord::Migration[4.2]
     def change
-      create_table :users do |t|
-        t.string :name
-        t.timestamps null: false
+      create_table :users do |table|
+        table.string :name
+        table.timestamps null: false
       end
     end
   end
