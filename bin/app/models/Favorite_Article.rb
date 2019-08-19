6686f32ec8ee54e038668653006616e1eb8bbf38
PollyCR <polly.c.reynolds@gmail.com>
2019-08-19 17:18:00 +0100

    added favorite article table and model

---
+++ bin/app/models/Favorite_Article.rb
@@ -0,0 +1,5 @@
+class FavoriteArticle < ActiveRecord::Base
+    belongs_to :users
+  
+    
+    end#class end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 5) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
+  create_table "articles", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.string "description"
+    t.text "url"
+    t.text "url_to_image"
+    t.datetime "published_at"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-  create_table "favorite_sources", force: :cascade do |t|
-    t.string "user_id"
-    t.string "source_id"
-    t.string "datetime"
-  end
-
-  create_table "sources", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.string "category"
   end
 
 end
