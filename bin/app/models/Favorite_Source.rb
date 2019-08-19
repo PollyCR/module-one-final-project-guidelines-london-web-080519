e50f5c7e45b530416bb4aecd80460a5c08d0498c
PollyCR <polly.c.reynolds@gmail.com>
2019-08-19 15:07:00 +0100

    misc typos and changes to syntax

--- bin/app/models/Favorite_Source.rb
+++ bin/app/models/Favorite_Source.rb
@@ -1,6 +1,7 @@
 class FavoriteSource < ActiveRecord::Base
 belongs_to :sources
 belongs_to :users
+has_many :articles
 
 
 end#class end
--- db/migrate/002_create_favorite_source.rb
+++ db/migrate/002_create_favorite_source.rb
@@ -1,9 +1,9 @@
 class CreateFavoriteSource < ActiveRecord::Migration[4.2]
     def change
-        create_table :favortie_sources do |table|
+        create_table :favorite_sources do |table|
             table.string :user_id
             table.string :source_id
-            table.string :datetime
+            table.timestamps
         end
     end
     end
