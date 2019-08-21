6c6c68b8da7a00cbbb246ac722b63a3d283bd0d9
PollyCR <polly.c.reynolds@gmail.com>
2019-08-21 20:50:11 +0100

    separating methods for cleaner repo

---
+++ app/get_methods.rb
@@ -0,0 +1,11 @@
+def get_articles(keyword_search) 
+    articles = JSON.parse(RestClient.get"https://newsapi.org/v2/everything?q=#{keyword_search}&from=2019-08-20&to=2019-08-20&language=en&apiKey=18f1d787fdf24f74b097f41574c6dbad")
+ end
+
+ def get_headlines(source_name)
+    source_for_headlines = Source.find{|source|source.name == source_name}
+    url = source_for_headlines.url.sub(/^https?\:\/\/(www.)?/,'')
+    # binding.pry
+    source_headlines = JSON.parse(RestClient.get"https://newsapi.org/v2/everything?domains=#{url}&language=en&apiKey=18f1d787fdf24f74b097f41574c6dbad")
+#  binding.pry
+ end 
