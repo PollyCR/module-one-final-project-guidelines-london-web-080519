ce9b3f91c8693beb96f871054f8f628b2b4a03d9
99chris99 <chrishodge87@gmail.com>
2019-08-21 09:21:52 +0100

    with start of save favorite sources methods

--- app/models/Article.rb
+++ app/models/Article.rb
@@ -9,7 +9,11 @@ class Article < ActiveRecord::Base
     
     def self.article_search_by_keyword(keyword)
        articles_with_keyword = Article.where("content LIKE ?"|| "title LIKE?", "%#{keyword}%")
-       binding.pry
+       
        return articles_with_keyword
     end
+
+    # def self.article_find_by_id(id)
+    #     article.find(id)
+    # end 
 end 
